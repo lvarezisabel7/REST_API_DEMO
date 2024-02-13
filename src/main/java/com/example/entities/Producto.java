287998f1a22c76dcc60eb6156930b243004f4315
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-13 11:54:52 +0100

    Add anotaciones en Producto

--- src/main/java/com/example/entities/Producto.java
+++ src/main/java/com/example/entities/Producto.java
@@ -11,6 +11,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,9 +34,19 @@ public class Producto implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotNull(message = "El nombre no puede ser nulo")
+    @NotEmpty(message = "El nombre del producto no puede estar vacio")
+    @Size(min = 4, max = 25,  message = "El nombre no puede tener menor de 4 caracteres ni mas de 25")
     private String name;
+
+    @NotBlank(message = "La descripcion del producto es requerida")
     private String description;
+
+    @Min(value = 0, message = "El precio no puede ser negativo")
     private int stock;
+
+    @Min(value = 0, message = "El precio no puede ser negativo")
     private int price;
 
     @ManyToOne(fetch = FetchType.LAZY)
