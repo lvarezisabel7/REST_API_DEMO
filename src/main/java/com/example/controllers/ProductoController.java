91125e643c2dd030258c730c17a43e898e39dc7b
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-13 11:41:59 +0100

    Finish metodo

--- src/main/java/com/example/controllers/ProductoController.java
+++ src/main/java/com/example/controllers/ProductoController.java
@@ -1,11 +1,18 @@
 package com.example.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.Producto;
@@ -20,14 +27,34 @@ public class ProductoController {
 
     private final ProductoService productoService;
 
-    // Metodo que devuelve todos los productos en formato JSON
+    // El metodo responde a una request del tipo http://localhost:8080/productos?page=0&size=3
+    // Si no se especifica page y size entonces que devuleve los productos ordenados por el nombre, por ejemplo
     @GetMapping
-    public ResponseEntity<List<Producto>> findAll() { 
+    public ResponseEntity<List<Producto>> findAll(
+        @RequestParam(name = "page", required = false) Integer page,
+        @RequestParam(name = "size", required = false) Integer size) { 
 
-        var productos = productoService.findAll();
+        ResponseEntity<List<Producto>> responseEntity = null;
+        Sort sortByName = Sort.by("name"); // criterio de ordenacion q le voy a pasar a la hora de crear el pageable
+        List<Producto> productos = new ArrayList<>();
 
-        ResponseEntity<List<Producto>> responseEntity = new ResponseEntity<List<Producto>>(productos, HttpStatus.OK);
+        // Comprobamos si han envidado page y size
+        if(page != null && size != null) {
+
+            // Queremos devolver los productos paginados
+            Pageable pageable = PageRequest.of(page, size, sortByName);
+            Page<Producto> pageProductos = productoService.findAll(pageable);
+            productos = pageProductos.getContent();
+            // Tenemos que devolver un obj del tipo responseEntity
+            // para ello llamamos al constructor
+            responseEntity = new ResponseEntity<List<Producto>>(productos, HttpStatus.OK);
+        } else {
+            // Solo ordenamiento
+            productos = productoService.findAll(sortByName);
+            responseEntity = new ResponseEntity<List<Producto>>(productos, HttpStatus.OK);
+        }
         return responseEntity;
+        
     }
 
 }
